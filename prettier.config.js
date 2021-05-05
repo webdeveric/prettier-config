ae10445e81bc46d0dee71fb8c567fefc27d829a2
Eric King <eric@webdeveric.com>
2021-05-05 16:44:40 -0500

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'never',
+};
---
+++ readme.md
@@ -0,0 +1,11 @@
+# My Prettier config
+
+## Install
+
+```sh
+npm install @webdeveric/prettier-config -D
+```
+
+## Configure your project
+
+[Read the docs](https://prettier.io/docs/en/configuration.html#sharing-configurations)
